03db90a6a1a83e8cbea3e6316a4401f4fc1e9c0a
xzd <xzd16@sohu.com>
2014-03-13 02:10:41 +0800

    增加生成index xml功能

--- parse_excel.py
+++ parse_excel.py
@@ -1,4 +1,7 @@
+# -*- coding: utf-8 -*-
+
 import sys
+import os
 import xlrd
 import lxml.etree as et
 
@@ -56,9 +59,21 @@ tab_dic={
 58:["RUN_SOURCE_9998","DATA_PERSONINFO","PERSONINFO_tb","personinfo","true","true","true","true","true","true"]
 }
 
+path=".\\xml"
+filename_fmt=path+"\\fmt.xml"
+filename_index=path+"\\index.xml"      
+def my_mkdir(path):
 
-         
-       
+    path=path.strip()
+    path=path.rstrip("\\")
+    isexist=os.path.exists(path)
+ 
+    if not isexist:        
+        os.makedirs(path)
+        return True
+    else:
+        return False
+
 #read excel file.
 def open_excel(file = "excel.xls"):
     try:
@@ -73,88 +88,119 @@ def excel_table_byindex(file="excel.xls", rownameindex=0, by_index=0):
     table = data.sheets()[by_index]
     nrows = table.nrows
     ncols = table.ncols
-    
     list = []
     for rownum in range(2,nrows):
         row = table.row_values(rownum)
         if row[2]!="":
             app = []
             for i in range(ncols):
-                app = row[2:8]
+                app = row[2:17]
             list.append(app)
     return list
+    
 
 #convert the excel file to xml file.
-def excel_to_xml():
-
-    file_name=open("test.xml", "r+")
-    root = et.Element("MESSAGE", attrib={"key":"", "description":""})
+def create_xml_file():
+    
+    file_name_fmt=open(filename_fmt, "wb+")  
+    root_fmt = et.Element("MESSAGE", attrib={"key":"", "description":""})
+    
     
     #read expand,mark,common fields.
     
-    L = excel_table_byindex("excel.xls",2,14)
-    run_expand=et.SubElement(root,"RUN_EXPAND_FIELDS",attrib={"key":"", "description":""})
+    L = excel_table_byindex("excel.xls",2,3)
+    run_expand=et.SubElement(root_fmt,"RUN_EXPAND_FIELDS",attrib={"key":"", "description":""})
     for i in range(6):
         item=et.SubElement(run_expand,"ITEM")
-        for j in range(len(L[i])):
-            if type(L[i][j])=="float":
-                item.set(item_attrib[j],int(unicode(L[i][j]).strip()))
-            else:
-                item.set(item_attrib[j],unicode(L[i][j]).strip())
-            for k in range(len(L[i]),len(item_attrib)):
+        for j in range(6):  
+            item.set(item_attrib[j],unicode(L[i][j]).strip())
+        for k in range(6,len(item_attrib)):
                 item.set(item_attrib[k],"")
-                    
-    mark_fields=et.SubElement(root,"MARK_FIELDS",attrib={"key":"", "description":""})    
+                            
+    mark_fields=et.SubElement(root_fmt,"MARK_FIELDS",attrib={"key":"", "description":""})    
     for i in range(6,len(L)):
         item=et.SubElement(mark_fields,"ITEM")
-        for j in range(len(L[i])):
-            if type(L[i][j])=="float":
-                item.set(item_attrib[j],int(unicode(L[i][j]).strip()))
-            else:
-                item.set(item_attrib[j],unicode(L[i][j]).strip())
-            for k in range(len(L[i]),len(item_attrib)):
+        for j in range(6):
+            item.set(item_attrib[j],unicode(L[i][j]).strip())
+        for k in range(6,len(item_attrib)):
                 item.set(item_attrib[k],"")
-
+            
     L = excel_table_byindex("excel.xls",2,15)
-    protocol_common=et.SubElement(root,"PROTOCOL_COMMON_FIELDS",attrib={"key":"", "description":""})
+    protocol_common=et.SubElement(root_fmt,"PROTOCOL_COMMON_FIELDS",attrib={"key":"", "description":""})
     for i in range(len(L)):
         item=et.SubElement(protocol_common,"ITEM")
-        for j in range(len(L[i])):
-            if type(L[i][j])=="float":
-                item.set(item_attrib[j],int(unicode(L[i][j]).strip()))
-            else:
-                item.set(item_attrib[j],unicode(L[i][j]).strip())
-            for k in range(len(L[i]),len(item_attrib)):
-                item.set(item_attrib[k],"")
-
+        for j in range(6):          
+            item.set(item_attrib[j],unicode(L[i][j]).strip())
+        for k in range(6,len(item_attrib)):
+                item.set(item_attrib[k],"")   
+           
     #read private fields of each protocol.
                 
-    protocol_private=et.SubElement(root,"PROTOCOL_PRIVATE_FIELDS",attrib={"key":"", "description":""})
+    protocol_private=et.SubElement(root_fmt,"PROTOCOL_PRIVATE_FIELDS",attrib={"key":"", "description":""})
+
+
+    file_name_index=open(filename_index, "wb+")
+    root_index = et.Element("MESSAGE")
+    dataset=et.SubElement(root_index,"DATASET",attrib={"name":"WA_COMMON_010017", "rmk":""})
+    data=et.SubElement(dataset,"DATA")
+    dataset1=et.SubElement(data,"DATASET",attrib={"name":"WA_COMMON_010013", "rmk":""})
+
+    
     for index in range(16,58):     
         print "%s: %s" % (tab_dic[index][0],tab_dic[index][3])
         
-        L = excel_table_byindex("excel.xls",2,index)
+        L1 = excel_table_byindex("excel.xls",2,index)
         table=et.SubElement(protocol_private,"TABLE")
         for i in range(len(table_attrib)):
             table.set(table_attrib[i],tab_dic[index][i].strip())
 
         for i in range(len(L)):
             item=et.SubElement(table,"ITEM")
-            for j in range(len(L[i])):
-                if type(L[i][j])=="float":
-                    item.set(item_attrib[j],int(unicode(L[i][j]).strip()))
-                else:
-                    item.set(item_attrib[j],unicode(L[i][j]).strip())
-                for k in range(len(L[i]),len(item_attrib)):
-                    item.set(item_attrib[k],"")
+            for j in range(6):               
+                item.set(item_attrib[j],unicode(L[i][j]).strip())
+            for k in range(6,len(item_attrib)):
+                item.set(item_attrib[k],"")
 
-    file_name.write(et.tounicode(root,pretty_print=True).encode('utf8'))
-    print "create xml file ok!"
-    file_name.close()
+        
+        data1=et.SubElement(dataset1,"DATA")
+        item=et.SubElement(data1,"ITEM",attrib={"key":"I010032", "val":"", "rmk":""})
+        item=et.SubElement(data1,"ITEM",attrib={"key":"I010033", "val":"", "rmk":""})
+        item=et.SubElement(data1,"ITEM",attrib={"key":"A010004", "val":unicode(tab_dic[index][3]).strip(), "rmk":""})
+        item=et.SubElement(data1,"ITEM",attrib={"key":"B050016", "val":"111", "rmk":""})
+        item=et.SubElement(data1,"ITEM",attrib={"key":"F010008", "val":"441900", "rmk":""})
+        item=et.SubElement(data1,"ITEM",attrib={"key":"I010038", "val":"1", "rmk":""})
+        item=et.SubElement(data1,"ITEM",attrib={"key":"I010039", "val":"UTF-8", "rmk":""})
+        dataset2=et.SubElement(data1,"DATASET",attrib={"name":"WA_COMMON_010014", "rmk":""})
+        data2=et.SubElement(dataset2,"DATA")
+        item=et.SubElement(data2,"ITEM",attrib={"key":"H040003", "val":"", "rmk":""})
+        item=et.SubElement(data2,"ITEM",attrib={"key":"H010020", "val":"", "rmk":""})
+        item=et.SubElement(data2,"ITEM",attrib={"key":"I010034", "val":"", "rmk":""})
+        dataset3=et.SubElement(data1,"DATASET",attrib={"name":"WA_COMMON_010015", "rmk":""})
+        data3=et.SubElement(dataset3,"DATA")
+        
+        for i in range(len(L)):
+            if unicode(L[i][12]).strip()!="":
+                item=et.SubElement(data3,"ITEM",attrib={"name":unicode(L[i][12]).strip(),"key":unicode(L[i][1]).strip(), "val":"", "rmk":unicode(L[i][2]).strip()})
+        L1 = excel_table_byindex("excel.xls",2,index)
+        for i in range(len(L1)):
+            if unicode(L1[i][12]).strip()!="":
+                item=et.SubElement(data3,"ITEM",attrib={"name":unicode(L1[i][12]).strip(),"key":unicode(L1[i][1]).strip(), "val":"", "rmk":unicode(L1[i][2]).strip()})
+                    
+                                   
+    file_name_fmt.write('<?xml version="1.0" encoding="UTF-8" ?>\n'.encode("utf8"))
+    file_name_fmt.write(et.tounicode(root_fmt,pretty_print=True).encode('utf8'))
+    print "create fmt xml file ok!"
+    file_name_fmt.close()
 
+    file_name_index.write('<?xml version="1.0" encoding="UTF-8" ?>\n'.encode("utf8"))
+    file_name_index.write(et.tounicode(root_index,pretty_print=True).encode('utf8'))
+    print "create index xml file ok!"
+    file_name_index.close()
 
 def main():
-    excel_to_xml()
+    
+    my_mkdir(path)
+    create_xml_file()
     
 if __name__=="__main__":
     main()
